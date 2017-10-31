606e27d58671910741cb19deebafacae2b79bbd7
bodhello <bodhi.alarcon@gmail.com>
2017-10-31 12:11:37 -0500

    add information on item-listing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -24,12 +24,14 @@ export class HomePage {
   	item0.rating = 4.4;
   	item0.person_name = 'Swetha';
   	item0.credit = 25;
+    item0.image_url = "../../assets/imgs/vacuum.jpg";
 
   	let item1 = new ItemListingComponent();
   	item1.name = 'Portable Grill';
   	item1.rating = 4.4;
   	item1.person_name = 'Swetha';
   	item1.credit = 20;
+    item1.image_url = "../../assets/imgs/grill.jpg";
 
   	this.items[0] = item0;
   	this.items[1] = item1;
