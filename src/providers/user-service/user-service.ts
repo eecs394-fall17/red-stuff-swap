f6f061dd007bf4b13689a23b8c9da2afc587578c
Encode.X <encode.xii@gmail.com>
2017-11-12 18:45:28 -0600

    added 2 new fake users

---
+++ src/providers/user-service/user-service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+/*
+ Generated class for the UserService provider.
+
+ See https://angular.io/guide/dependency-injection for more info on providers
+ and Angular DI.
+ */
+@Injectable()
+export class UserService {
+
+  private _userA = {user_id: 0, user_name: 'Donnie River', user_email: 'driver@user.mail.com'};
+  private _userB = {user_id: 1, user_name: 'Annie Curtis', user_email: 'acurtis@user.mail.com'};
+  private _currentUser: any;
+
+  constructor() {
+    this._currentUser = this._userA;
+  }
+
+  public switchUser(){
+    this._currentUser = this._currentUser == this._userA ? this._userB:this._userA;
+  }
+
+  // everyone want to use a true email address to test with
+  public changeUserInfo(data){
+    this._userA = data.userA;
+    this._userB = data.userB;
+  }
+
+  public getCurrentUser(){
+    return this._currentUser;
+  }
+
+  public getUsers(){
+    return {userA: this._userA, userB: this._userB};
+  }
+}
