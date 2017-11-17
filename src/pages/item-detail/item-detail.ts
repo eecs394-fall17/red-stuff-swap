82cd4d90b7f6c4d5a023dc978071262190e26151
Encode.X <encode.xii@gmail.com>
2017-11-16 18:55:30 -0600

    Borrower can pick date range

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -5,6 +5,7 @@ import {AngularFireDatabase, AngularFireList} from "angularfire2/database";
 import * as firebase from "firebase";
 import {UserService} from "../../providers/user-service/user-service";
 import { Observable } from "rxjs/Observable";
+import * as moment from 'moment';
 /**
  * Generated class for the ItemDetailPage page.
  *
@@ -21,37 +22,63 @@ export class ItemDetailPage {
 
 	item: any;
   stars = ["star", "star", "star", "star", "star"];
-  _items2: Observable<any[]>;
 
   requested = false;
+  requesting = false;
+
+  private _orderRef: AngularFireList<any>;
+  private _orders: Observable<any[]>;
+  private _userRelatedOrders: Observable<any[]>;
+  private _relatedOrders: Observable<any[]>;
+
   private fromDate: string;
+  private fromDateRaw: Date;
   private fromTime: string;
   private toDate: string;
+  private toDateRaw: Date;
   private toTime: string;
-  private createTime: number;
-  private item_id: string;
-  private _itemRef: AngularFireList<any>;
-  private fromDate_1: number;
-  private toDate_1: number;
-  private borrower_id: number;
-  private owner_id: string;
-  private number: number;
-  private item_id_1: string;
-  private hourAndMinute: string;
+
+  // private createTime: number;
+  // private item_id: string;
+  // private fromDate_1: number;
+  // private toDate_1: number;
+  // private borrower_id: number;
+  // private owner_id: string;
+  // private number: number;
+  // private item_id_1: string;
+  // private hourAndMinute: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private modalCtrl: ModalController,
-              private toastCtrl: ToastController, private db: AngularFireDatabase) {
+              private toastCtrl: ToastController, private db: AngularFireDatabase, private user: UserService) {
   	this.item= this.navParams.get('item');
-  	this.number = 0;
-    let us = new UserService();
-    this.borrower_id = us.getCurrentUser().user_id;
-    this._itemRef = this.db.list('/order');
-    this._items2 = this._itemRef.snapshotChanges().map(changes => {
-      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
-    });
-    var ref = firebase.database().ref("/order");
-    ref.orderByChild('borrower_id').equalTo(1).on("child_added", function(snapshot) {
-      console.log(snapshot.key);
+
+    this._orderRef = this.db.list('/order');
+    // this._orders = this._orderRef.snapshotChanges().map(changes => {
+    //   return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    // });
+
+    this.db.list('/order',
+      ref => ref.orderByChild(`borrower_id`).equalTo(user.getCurrentUser().user_id))
+      .snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val()}));
+    }).subscribe(orders => {
+      const now = Date.now();
+      for (let i = 0; i < orders.length; i++){
+        let order = orders[i];
+        if (order.start_time > now){
+          this.requested = true;
+          const from = new Date(order.start_time);
+          const to = new Date(order.end_time);
+
+          this.fromTime = new Date(from.valueOf() - from.getTimezoneOffset() * 60000).toISOString();
+          this.toTime = new Date(to.valueOf() - from.getTimezoneOffset() * 60000).toISOString();
+          this.fromDate = `${from.getMonth() + 1}/${from.getDate()}/${from.getFullYear()}`;
+          this.fromDateRaw = new Date(this.fromDate);
+          this.toDate = `${to.getMonth() + 1}/${to.getDate() + 1}/${to.getFullYear()}`;
+          this.toDateRaw = new Date(this.toDate);
+          break;
+        }
+      }
     });
   }
 
@@ -75,38 +102,44 @@ export class ItemDetailPage {
     this.fromTime = new Date(Date.now() - timer.getTimezoneOffset() * 60000).toISOString();
     this.toTime = new Date(Date.now() - timer.getTimezoneOffset() * 60000).toISOString();
     this.fromDate = `${timer.getMonth() + 1}/${timer.getDate()}/${timer.getFullYear()}`;
-    this.toDate = `${timer.getMonth() + 1}/${timer.getDate()}/${timer.getFullYear()}`;
-    this.createTime = timer.getTime();
-    this.fromDate_1 = timer.getTime();
-    this.toDate_1 = timer.getTime();
-    this.owner_id = this.item.person_id;
-    this.item_id_1 = this.item.key;
+    this.fromDateRaw = new Date(this.fromDate);
+    this.toDate = `${timer.getMonth() + 1}/${timer.getDate() + 1}/${timer.getFullYear()}`;
+    this.toDateRaw = new Date(this.toDate);
   }
 
   requestItem(){
-    if(this.number == 0) {
-      this.number = 1;
-      this.requested = !this.requested;
-      this._itemRef.push({
-        item_id: this.item.key,
-        end_date: this.toDate_1,
-        time_created: this.createTime,
-        owner_id: this.owner_id,
-        start_date: this.fromDate_1,
-        status: 'request_sent',
-        borrower_id: this.borrower_id,
-      });
-    }
-    else if(this.number == 1) {
-      this.presentToast(`You have sent a request.`);
-    }
     // todo use Date.parse('Thu, 01 Jan 1970 00:00:00 GMT-0400') to parse time data
     // source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
+
+    let fTime = new Date(this.fromTime);
+    let tTime = new Date(this.toTime);
+    let from = moment(`${this.fromDateRaw.toLocaleDateString()} ${fTime.getUTCHours()}:${fTime.getUTCMinutes()}`,
+      `MM/DD/YYYY HH:mm`);
+    let to = moment(`${this.toDateRaw.toLocaleDateString()} ${tTime.getUTCHours()}:${tTime.getUTCMinutes()}`,
+      `MM/DD/YYYY HH:mm`);
+    this.requesting = true;
+    this._orderRef.push({
+      borrower_id: this.user.getCurrentUser().user_id,
+      lender_id: this.item.person_id,
+      item_id: this.item.key,
+      status: `requested`,
+      start_time: from.valueOf(),
+      end_time: to.valueOf(),
+      time_created: Date.now()
+    }).then(()=>{
+      this.requesting = false;
+      this.requested = true;
+    });
   }
 
-  openCalendar(label) {
+  openCalendar() {
     const options: CalendarModalOptions = {
-      title: 'PICK DATE',
+      title: 'PICK BORROW DATE',
+      pickMode: 'range',
+      defaultDateRange: {
+        from: this.fromDateRaw,
+        to: this.toDateRaw
+      }
     };
     let myCalendar =  this.modalCtrl.create(CalendarModal, {
       options: options
@@ -114,19 +147,14 @@ export class ItemDetailPage {
 
     myCalendar.present();
 
-    myCalendar.onDidDismiss((date, role) => {
-      if(date != null){
-        if(label == 'from'){
-          this.fromDate = `${date.months}/${date.date}/${date.years}`;
-          var date1 = `${date.years}-${date.months}-${date.date} ${date.hour}:${date.minute}`;
-          var date2 = new Date(Date.parse(date1.replace(/-/g, "/")));
-          this.fromDate_1 = date2.getTime();
-        }else{
-          this.toDate = `${date.months}/${date.date}/${date.years}`;
-          var date3 = `${date.years}-${date.months}-${date.date}  ${date.hour}:${date.minute}`;
-          var date4 = new Date(Date.parse(date3.replace(/-/g, "/")));
-          this.fromDate_1 = date4.getTime();
-        }
+    myCalendar.onDidDismiss((dates, role) => {
+      if(dates != null){
+        let from = dates.from;
+        let to = dates.to;
+        this.fromDate = `${from.months}/${from.date}/${from.years}`;
+        this.fromDateRaw = from.dateObj;
+        this.toDate = `${to.months}/${to.date}/${to.years}`;
+        this.toDateRaw = to.dateObj;
       }
     })
   }
